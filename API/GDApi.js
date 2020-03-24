ac48d3606ad77f982ea47aa0b4fa95aebc313259
arthuro555 <arthur.pacaud@hotmail.fr>
2020-03-24 10:42:11 +0100

    [WIP] Made basic API beginning

--- API/GDApi.js
+++ API/GDApi.js
@@ -2,4 +2,57 @@
  * The namespace containing the whole modding API
  * @namespace
  */
-window.GDApi = {};
+window.GDAPI = {};
+
+/**
+ * GD Callback type enum.
+ * @enum
+ */
+GDAPI.CALLBACKS = {
+    FIRST_SCENE_LOADED: 0,
+    SCENE_LOADED: 1,
+    PRE_EVENTS: 2,
+    POST_EVENTS: 3,
+    SCENE_PAUSED: 4,
+    SCENE_RESUMED: 5,
+    SCENE_UNLOADING: 6,
+    SCENE_UNLOADED: 7,
+    OBJECT_DELETED_FROM_SCENE: 8
+}
+
+/**
+ * Registers Callback functions.
+ * @param {GDAPI.CALLBACKS} callbackType - The event on which you want your callback to be called.
+ * @param {Function} callback - The callback to register.
+ */
+GDAPI.registerCallback = function(callbackType, callback) {
+    if(callbackType === GDAPI.CALLBACKS.FIRST_SCENE_LOADED) { callbacksFirstRuntimeSceneLoaded.push(callback); }
+    if(callbackType === GDAPI.CALLBACKS.SCENE_LOADED) { callbacksRuntimeSceneLoaded.push(callback); }
+    if(callbackType === GDAPI.CALLBACKS.PRE_EVENTS) { callbacksRuntimeScenePreEvents.push(callback); }
+    if(callbackType === GDAPI.CALLBACKS.POST_EVENTS) { callbacksRuntimeScenePostEvents.push(callback); }
+    if(callbackType === GDAPI.CALLBACKS.SCENE_PAUSED) { callbacksRuntimeScenePaused.push(callback); }
+    if(callbackType === GDAPI.CALLBACKS.SCENE_RESUMED) { callbacksRuntimeSceneResumed.push(callback); }
+    if(callbackType === GDAPI.CALLBACKS.SCENE_UNLOADING) { callbacksRuntimeSceneUnloading.push(callback); }
+    if(callbackType === GDAPI.CALLBACKS.SCENE_UNLOADED) { callbacksRuntimeSceneUnloaded.push(callback); }
+    if(callbackType === GDAPI.CALLBACKS.OBJECT_DELETED_FROM_SCENE) { callbacksObjectDeletedFromScene.push(callback); }
+}
+
+/**
+ * The current Instance of RuntimeScene.
+ * @type {gdjs.RuntimeScene}
+ */
+GDAPI.currentScene = null;
+
+/**
+ * The Instance of RuntimeGame.
+ * @type {gdjs.RuntimeGame}
+ */
+GDAPI.game = null;
+
+// Polyfill Object registration to support Older games
+gdjs.RuntimeScene.prototype.registerObject = gdjs.RuntimeScene.prototype.registerObject || function(objectData) {
+    this._objects.put(objectData.name, objectData);
+    this._instances.put(objectData.name, []); //Also reserve an array for the instances
+    this._instancesCache.put(objectData.name, []); //and for cached instances
+    this._objectsCtor.put(objectData.name, gdjs.getObjectConstructor(objectData.type)); //And cache the constructor for the performance sake
+}
---
+++ API/SceneTools.js
@@ -0,0 +1,14 @@
+/**
+ * Tools to interract with scenes.
+ * @memberof GDAPI
+ * @namespace
+ */
+GDAPI.SceneTools = {}
+
+/**
+ * Add an object to the Scene and take care of registration.
+ * @param {gdjs.RuntimeObject} - The object to add to the scene.
+ */
+GDAPI.SceneTools.addObjectToScene = function (runtimeObject) {
+    //TODO
+}
