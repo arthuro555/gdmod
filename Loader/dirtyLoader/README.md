0b97725d10b2e516f4b65b46defb4c3f8ea85306
arthuro555 <arthur.pacaud@hotmail.fr>
2020-04-12 20:51:09 +0200

    Add basic working Loader

---
+++ Loader/dirtyLoader/README.md
@@ -0,0 +1,3 @@
+# Dirty Loader
+
+This is the current loader: It just overwrites the current GDJS Runtime of the game with the patched one. It's not the best loader as it doesn't uses patches and can break too old/new versions (by not including a new function or containing one with breaking changes).
---
+++ Loader/dirtyLoader/loader.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+
+let insertLine = function(line, position, text) {
+    /** @type {Array<string>} */
+    let arrayText = text.split("\n");
+    arrayText.splice(position, 0, line);
+    let newText = "";
+    for(let newLine of arrayText) {
+        newText += newLine + "\n";
+    }
+    return newText;
+}
+
+module.exports = function(outputDir) {
+    // Basic check for a GDevelop game
+    if (!fs.readdirSync(outputDir).includes("gd.js")) { console.error("The given output path is not a GDevelop game!") }
+
+    // List recursively all API files
+    let allfiles = [];
+    findFiles = (dir) => {
+        files = fs.readdirSync(dir);
+        for(let file of files) {
+            if(file.includes("git")) {continue;} // Ignore Version Control files
+            if (fs.statSync(dir + "/" + file).isDirectory()) {
+                findFiles(dir + "/" + file);
+            } else {
+                allfiles.push(path.join(dir, "/", file));
+            }
+        }
+    }
+    findFiles(path.join("..", "..", "API"));
+
+    // Copy files over
+    copyFiles = function(resolve) {
+        for(let file of allfiles) {
+            fs.readFile(file, (error, fileInMemory) => {
+                let endpath = path.join(outputDir, file.replace(path.join("..", "..", "API/"), '').replace(path.join("patched_gdjs", ""), ""));
+                console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.green("Patching file ") + chalk.italic(chalk.grey(endpath)) + chalk.green("..."));
+                fs.writeFile(endpath, fileInMemory, () => {});
+            });
+        }
+        resolve();
+    }
+
+    new Promise(copyFiles).then(() => {
+        // Compatibility Patch
+        return new Promise((resolve) => {
+            fs.readFile(path.join(outputDir, "index.html"), (err, indexFile) => {
+                indexFile = String(indexFile)       
+                .replace("gdjs.registerObjects();", '')
+                .replace("gdjs.registerBehaviors();", '')
+                .replace("gdjs.registerGlobalCallbacks();", '');
+                fs.writeFile(path.join(outputDir, "index.html"), indexFile, () => {resolve();});
+                console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied additional compatibility patch to index.html."));
+            });
+        });
+        
+    }).then(() => {
+        // Add Includes for API
+
+        let indexFile = String(fs.readFileSync(path.join(outputDir, "index.html")));
+        // An include file is used to determine the loading order and what needs to be loaded.
+        let APIFiles = JSON.parse(String(fs.readFileSync(path.join("..", "..", "API", "includes.json"))));
+        for(let include of APIFiles.reverse()) {
+            indexFile = insertLine("	<script src=\"" + include.split(path.sep).slice(-1)[0]  + "\"></script>", 70, indexFile);
+        }
+        fs.writeFileSync(path.join(outputDir, "index.html"), indexFile)
+        console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied dependency includes patch to index.html."));
+    });
+}
+
+module.exports(path.join(".", "h"));
