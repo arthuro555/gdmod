31373ce53dbd5a01bf551f357e15610838d6e5bc
arthuro555 <arthur.pacaud@hotmail.fr>
2020-04-13 17:39:47 +0200

    Add Base Loader to the cli
    and some fixes here and there. Removed the fs argument from loader as I cannot use it as I wanted to.

--- Loader/dirtyLoader/loader.js
+++ Loader/dirtyLoader/loader.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const chalk = require("chalk");
+const fs = require("fs");
 
 let insertLine = function(line, position, text) {
     /** @type {Array<string>} */
@@ -15,69 +16,71 @@ let insertLine = function(line, position, text) {
 /**
  * Applies patches to a GD game
  * @param {string} outputDir - The directory of the GDevelop game.
- * @param {any} [fs] - The filesystem object to use.
  */
-module.exports = function(outputDir, fs) {
-    if(!fs) {
-        const fs = require("fs");
-    }
+module.exports = function(outputDir) {
+    return new Promise((mainResolve, reject) => {
+        // Basic check for a GDevelop game
+        if (!fs.readdirSync(outputDir).includes("gd.js")) { console.error(chalk.redBright("The given output path is not a GDevelop game!")); reject(); return; }
 
-    // Basic check for a GDevelop game
-    if (!fs.readdirSync(outputDir).includes("gd.js")) { console.error(chalk.redBright("The given output path is not a GDevelop game!")); return; }
+        // Basic check for an existing patch
+        if (fs.readdirSync(outputDir).includes("GDApi.js")) { console.error(chalk.redBright("The given output path contains an already patched game!")); reject(); return; }
 
-    // Basic check for an existing patch
-    if (!fs.readdirSync(outputDir).includes("GDApi.js")) { console.error(chalk.redBright("The given output path contains an already patched game!")); return; }
-
-    // List recursively all API files
-    let allfiles = [];
-    findFiles = (dir) => {
-        files = fs.readdirSync(dir);
-        for(let file of files) {
-            if(file.includes("git")) {continue;} // Ignore Version Control files
-            if (fs.statSync(dir + "/" + file).isDirectory()) {
-                findFiles(dir + "/" + file);
-            } else {
-                allfiles.push(path.join(dir, "/", file));
+        // List recursively all API files
+        let allfiles = [];
+        findFiles = (dir) => {
+            files = fs.readdirSync(dir);
+            for(let file of files) {
+                if(file.includes("git")) {continue;} // Ignore Version Control files
+                if (fs.statSync(dir + "/" + file).isDirectory()) {
+                    findFiles(dir + "/" + file);
+                } else {
+                    allfiles.push(path.join(dir, "/", file));
+                }
             }
         }
-    }
-    findFiles(path.join("..", "..", "API"));
+        findFiles(path.join(__dirname, "..", "..", "API"));
 
-    // Copy files over
-    copyFiles = function(resolve) {
-        for(let file of allfiles) {
-            fs.readFile(file, (error, fileInMemory) => {
-                let endpath = path.join(outputDir, file.replace(path.join("..", "..", "API/"), '').replace(path.join("patched_gdjs", ""), ""));
-                console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.green("Patching file ") + chalk.italic(chalk.grey(endpath)) + chalk.green("..."));
-                fs.writeFile(endpath, fileInMemory, () => {});
-            });
+        // Copy files over
+        copyFiles = function(resolve) {
+            let finished = 0;
+            for(let file of allfiles) {
+                fs.readFile(file, (error, fileInMemory) => {
+                    let endpath = path.join(outputDir, file.replace(path.join(__dirname, "..", "..", "API/"), '').replace(path.join("patched_gdjs", ""), ""));
+                    console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.green("Patching file ") + chalk.italic(chalk.grey(endpath)) + chalk.green("..."));
+                    fs.writeFile(endpath, fileInMemory, () => {});
+
+                    if(++finished === allfiles.length) { resolve(); }
+                });
+            }
         }
-        resolve();
-    }
 
-    new Promise(copyFiles).then(() => {
-        // Compatibility Patch
-        return new Promise((resolve) => {
-            fs.readFile(path.join(outputDir, "index.html"), (err, indexFile) => {
-                indexFile = String(indexFile)       
-                .replace("gdjs.registerObjects();", '')
-                .replace("gdjs.registerBehaviors();", '')
-                .replace("gdjs.registerGlobalCallbacks();", '');
-                fs.writeFile(path.join(outputDir, "index.html"), indexFile, () => {resolve();});
-                console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied additional compatibility patch to index.html."));
+        new Promise(copyFiles).then(() => {
+            // Compatibility Patch
+            return new Promise((resolve) => {
+                fs.readFile(path.join(outputDir, "index.html"), (err, indexFile) => {
+                    indexFile = String(indexFile)       
+                    .replace("gdjs.registerObjects();", '')
+                    .replace("gdjs.registerBehaviors();", '')
+                    .replace("gdjs.registerGlobalCallbacks();", '');
+                    fs.writeFile(path.join(outputDir, "index.html"), indexFile, () => {resolve();});
+                    console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied additional compatibility patch to index.html."));
+                });
             });
+            
+        }).then(() => {
+            // Add Includes for API
+
+            let indexFile = String(fs.readFileSync(path.join(outputDir, "index.html")));
+            // An include file is used to determine the loading order and what needs to be loaded.
+            let APIFiles = JSON.parse(String(fs.readFileSync(path.join(__dirname, "..", "..", "API", "includes.json"))));
+            for(let include of APIFiles.reverse()) {
+                indexFile = insertLine("	<script src=\"" + include.split(path.sep).slice(-1)[0]  + "\"></script>", 70, indexFile);
+            }
+            fs.writeFileSync(path.join(outputDir, "index.html"), indexFile)
+            console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied dependency includes patch to index.html."));
+
+            // End of patching
+            mainResolve();
         });
-        
-    }).then(() => {
-        // Add Includes for API
-
-        let indexFile = String(fs.readFileSync(path.join(outputDir, "index.html")));
-        // An include file is used to determine the loading order and what needs to be loaded.
-        let APIFiles = JSON.parse(String(fs.readFileSync(path.join("..", "..", "API", "includes.json"))));
-        for(let include of APIFiles.reverse()) {
-            indexFile = insertLine("	<script src=\"" + include.split(path.sep).slice(-1)[0]  + "\"></script>", 70, indexFile);
-        }
-        fs.writeFileSync(path.join(outputDir, "index.html"), indexFile)
-        console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied dependency includes patch to index.html."));
-    });
+    })
 }
--- cli/cli.js
+++ cli/cli.js
@@ -1,47 +1,99 @@
 #! /usr/bin/env node
 
 const program = require("commander");
-var chalk = require('chalk');
-const GDJSPatcher = require("../Loader/patchUtils/patcher")
+const chalk = require('chalk');
+const fs = require("fs");
+const path = require("path");
+const tempdir = path.join(fs.realpathSync(require("os").tmpdir()), "GDModTemp");
+const asar = require("asar");
+const GDJSPatcher = require("../Loader/dirtyLoader/loader");
 
 program
     .name("gdmodCli")
     .version('0.0.1')
-    .description("A CLI for installing the mod loader or a mod")
+    .description("A CLI for patchimg a game or adding a mod");
 
 program
     .command('install-mod-unpacked <directory>')
-    .description('Install a mod in an unpacked GDevelop game with the loader')
+    .description('Install a mod in an unpacked and patched GDevelop game')
     .action((directory) => {
         //TODO
-        console.warn(chalk.redBright(chalk.bold("Error: Not Implemented")))
+        console.warn(chalk.redBright(chalk.bold("Error: Not Implemented")));
+        return false;
     })
 
 program
-    .command('install-loader-unpacked <directory> [additionalPatches]')
-    .description('Install the loader in an unpacked GDevelop game')
-    .action((directory, additionalPatches) => {
-        GDJSPatcher(directory, additionalPatches);
+    .command('install-loader-unpacked <directory>')
+    .description('Install the loader and apply patches to an unpacked GDevelop game')
+    .action((directory) => {
+        GDJSPatcher(directory);
+        return true;
     })
 
 program
     .command('install-mod-asar <asarFile>')
-    .description('Install a mod in a GDevelop game with the loader')
+    .description('Install a mod in a patched GDevelop game')
     .action((asarFile) => {
         //TODO
-        console.warn(chalk.redBright(chalk.bold("Error: Not Implemented")))
+        console.warn(chalk.redBright(chalk.bold("Error: Not Implemented")));
+        return false;
     })
 
 program
     .command('install-loader-asar <asarFile>')
-    .description('Install the loader in a GDevelop game')
-    .action((asarFile) => {
-        //TODO
-        console.warn(chalk.redBright(chalk.bold("Error: Not Implemented")))
+    .option("-d, --debug", "Activate debug output")
+    .description('Install the loader and apply patches to a GDevelop game')
+    .action((asarFile, args) => {
+        console.log(chalk.greenBright(chalk.italic("Loading asar file...")));
+        
+        // Make sure temp directory is empty
+        try {
+            fs.mkdirSync(tempdir);
+        } catch {
+            console.log(chalk.blue(chalk.italic("Cleaning up old files...")));
+            fs.rmdirSync(tempdir, { recursive: true });
+            fs.mkdirSync(tempdir);
+        }
+
+        // Copy asar to temp folder
+        fs.writeFileSync(path.join(tempdir, "app.asar"), fs.readFileSync(asarFile));
+
+        console.log(chalk.greenBright(chalk.italic("Backing up old asar file...")));
+        fs.writeFileSync(asarFile + ".bak", fs.readFileSync(asarFile));
+
+        try {
+            console.log(chalk.greenBright(chalk.italic("Unpacking asar file...")));
+            asar.extractAll(path.join(tempdir, "app.asar"), tempdir);
+            fs.unlinkSync(path.join(tempdir, "app.asar")); // To not repack it later
+        } catch (e) {
+            if (args.debug) { console.log(e); }
+            console.error(chalk.redBright(chalk.bold("[ERROR] ") + "Invalid asar File!"));
+            return false;
+        }
+
+        console.log(chalk.greenBright(chalk.italic("Patching game data...")));
+        GDJSPatcher(path.join(tempdir, "app")).then(() => {
+            console.log(chalk.greenBright(chalk.italic("Repacking asar file...")));
+            asar.createPackage(tempdir, path.join(tempdir, "app.asar")).then(() => {
+                // Copy temporary new asar back to the original path
+                fs.writeFileSync(asarFile, fs.readFileSync(path.join(tempdir, "app.asar")));
+                console.log(chalk.greenBright(chalk.bold("DONE !")));
+                console.log(chalk.blue(chalk.italic("Cleaning up...")));
+                // Delete temp directory
+                fs.rmdirSync(tempdir, { recursive: true });
+            });
+        }, () => {
+            // Patch aborted, cleaning up:
+            console.log(chalk.blue(chalk.italic("Cleaning up...")));
+            // Delete temp directory
+            fs.rmdirSync(tempdir, { recursive: true });
+            return false;
+        });
+        return true;
     })
 
 program
     .command("*")
-    .outputHelp()
+    .outputHelp();
 
 program.parse(process.argv);
