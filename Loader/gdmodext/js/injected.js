a0a2f457c7328407edc2d3d47279b6a81699e218
arthuro555 <arthur.pacaud@hotmail.fr>
2020-06-08 19:26:56 +0200

    Change CDN Tag

--- Loader/gdmodext/js/injected.js
+++ Loader/gdmodext/js/injected.js
@@ -11,7 +11,7 @@ const debug = false;
 /**
  * The CDN to fetch the GDAPI files from
  */
-const CDN = "https://cdn.jsdelivr.net/gh/arthuro555/gdmod/API/";
+const CDN = "https://cdn.jsdelivr.net/gh/arthuro555/gdmod@0.0.1-preview/API/";
 
 /**
  * Flag telling if that page got patched already.
