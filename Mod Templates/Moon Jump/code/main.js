58385f93526bc5fa900542f4ec5f2e0baebe445e
arthuro555 <arthur.pacaud@hotmail.fr>
2020-06-11 13:55:30 +0200

    Add Moon Jump Mod Template

---
+++ Mod Templates/Moon Jump/code/main.js
@@ -0,0 +1,18 @@
+/**
+ * The moon jump mod.
+ * Allows to fly.
+ * @extends {GDAPI.Mod}
+ */
+class MoonJumpMod extends GDAPI.Mod {
+    preEvent(runtimeScene) {
+		runtimeScene._constructListOfAllInstances();
+        for(let gdobject of runtimeScene._allInstancesList) {
+			for(let behavior of gdobject._behaviors) {
+				if(behavior.type === "PlatformBehavior::PlatformerObjectBehavior") 
+				  behavior._canJump = true;
+			}
+		}
+    };
+}
+
+return MoonJumpMod;
