59a8f67e42a24d6696c66c1eecae7fc8a479e935
arthuro555 <arthur.pacaud@hotmail.fr>
2020-06-26 15:13:53 +0200

    Cleanup dirty loader

--- Loader/dirtyLoader/loader.js
+++ Loader/dirtyLoader/loader.js
@@ -18,43 +18,60 @@ let insertLine = function(line, position, text) {
  * @param {string} outputDir - The directory of the GDevelop game.
  */
 module.exports.installGDMod = function(outputDir) {
-    return new Promise((mainResolve, reject) => {
+        const outputDirFiles = fs.readdirSync(outputDir);
+        
         // Basic check for a GDevelop game
-        if (!fs.readdirSync(outputDir).includes("gd.js")) { console.error(chalk.redBright("The given output path is not a GDevelop game!")); reject(); return; }
+        if (!outputDirFiles.includes("gd.js")) { 
+            console.error(chalk.redBright("The given output path is not a GDevelop game!")); 
+            return Promise.reject("The given output path is not a GDevelop game!"); 
+        }
 
         // Basic check for an existing patch
-        if (fs.readdirSync(outputDir).includes("GDApi.js")) { console.error(chalk.redBright("The given output path contains an already patched game!")); reject(); return; }
+        if (outputDirFiles.includes("GDApi.js")) { 
+            console.error(chalk.redBright("The given output path contains an already patched game!")); 
+            return Promise.reject("The given output path contains an already patched game!"); 
+        }
 
         // List recursively all API files
-        let allfiles = [];
         findFiles = (dir) => {
-            files = fs.readdirSync(dir);
-            for(let file of files) {
-                if(file.includes("git")) {continue;} // Ignore Version Control files
-                if (fs.statSync(dir + "/" + file).isDirectory()) {
-                    findFiles(dir + "/" + file);
-                } else {
-                    allfiles.push(path.join(dir, "/", file));
+            return new Promise((resolve) => {
+                let allfiles = [];
+                finder = (dir) => {
+                    for(let file of fs.readdirSync(dir)) {
+                        const filePath = path.join(dir, file);
+                        if(file.includes("git")) {continue;} // Ignore Version Control files
+                        if (fs.statSync(filePath).isDirectory()) {
+                            finder(dir + "/" + file);
+                        } else {
+                            allfiles.push(path.join(dir, "/", file));
+                        }
+                    }
                 }
-            }
+                finder(dir);
+                fs.writeFileSync(path.join(__dirname, "log.txt"), allfiles);
+                resolve(allfiles);
+            });
         }
-        findFiles(path.join(__dirname, "..", "..", "API"));
 
         // Copy files over
-        copyFiles = function(resolve) {
-            let finished = 0;
-            for(let file of allfiles) {
-                fs.readFile(file, (error, fileInMemory) => {
-                    let endpath = path.join(outputDir, file.replace(path.join(__dirname, "..", "..", "API/"), '').replace(path.join("patched_gdjs", ""), ""));
-                    console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.green("Patching file ") + chalk.italic(chalk.grey(endpath)) + chalk.green("..."));
-                    fs.writeFile(endpath, fileInMemory, () => {});
+        copyFiles = function(allFiles) {
+            return new Promise((resolve) => {
+                let finished = 0;
+                for(let file of allFiles) {
+                    fs.readFile(file, (error, fileInMemory) => {
+                        let endpath = path.join(outputDir, file.replace(path.join(__dirname, "..", "..", "API/"), '').replace(path.join("patched_gdjs", ""), ""));
+                        console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.green("Patching file ") + chalk.italic(chalk.grey(endpath)) + chalk.green("..."));
+                        fs.writeFile(endpath, fileInMemory, () => {});
 
-                    if(++finished === allfiles.length) { resolve(); }
-                });
-            }
+                        if(++finished === allFiles.length) { resolve(); }
+                    });
+                }
+            });
         }
 
-        new Promise(copyFiles).then(() => {
+        return findFiles(path.join(__dirname, "..", "..", "API"))
+        .then(copyFiles)
+        .then(() => {
             // Compatibility Patch
             return new Promise((resolve) => {
                 fs.readFile(path.join(outputDir, "index.html"), (err, indexFile) => {
@@ -66,10 +83,8 @@ module.exports.installGDMod = function(outputDir) {
                     console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied additional compatibility patch to index.html."));
                 });
             });
-            
         }).then(() => {
             // Add Includes for API
-
             let indexFile = String(fs.readFileSync(path.join(outputDir, "index.html")));
             // An include file is used to determine the loading order and what needs to be loaded.
             let APIFiles = JSON.parse(String(fs.readFileSync(path.join(__dirname, "..", "..", "API", "includes.json"))));
@@ -78,9 +93,5 @@ module.exports.installGDMod = function(outputDir) {
             }
             fs.writeFileSync(path.join(outputDir, "index.html"), indexFile)
             console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied dependency includes patch to index.html."));
-
-            // End of patching
-            mainResolve();
-        });
-    })
+        })
 }
