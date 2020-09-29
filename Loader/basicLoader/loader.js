d4e3c420fe21f65132771d69bcac9eb85a0c4e72
arthuro555 <arthur.pacaud@hotmail.fr>
2020-09-29 20:36:28 +0200

    Run Prettier

--- Loader/basicLoader/loader.js
+++ Loader/basicLoader/loader.js
@@ -19,69 +19,100 @@ const APIDeps = require(APIDir + "/includes.json");
  * @param {string} text - The HTML to modify.
  * @param {string} include - The url to or the script to add.
  */
-const insertInclude = function(text, include) {
-    const n = text.lastIndexOf("<script src=");
-    let originalInclude = text.slice(n).match(/<script src=.*>.*<\/script>/gm);
-    originalInclude = originalInclude[originalInclude.length - 1];
-    return text.slice(0, n) + text.slice(n).replace(
-        /<script src=.*>.*<\/script>/gm, 
+const insertInclude = function (text, include) {
+  const n = text.lastIndexOf("<script src=");
+  let originalInclude = text.slice(n).match(/<script src=.*>.*<\/script>/gm);
+  originalInclude = originalInclude[originalInclude.length - 1];
+  return (
+    text.slice(0, n) +
+    text
+      .slice(n)
+      .replace(
+        /<script src=.*>.*<\/script>/gm,
         originalInclude + `\n\t<script src="${include}"></script>`
-    );
-}
+      )
+  );
+};
 
 /**
  * Installs GDAPI in a GDevelop HTML5 game.
  * @param {string} outputDir - The directory of the GDevelop game.
  */
-module.exports.installGDMod = function(outputDir) {
-        const outputDirFiles = fs.readdirSync(outputDir);
-        
-        // Check if it is a GDevelop game
-        if (!outputDirFiles.includes("gd.js")) { 
-            console.error(chalk.redBright("The given output path is not a GDevelop game!")); 
-            return Promise.reject("The given output path is not a GDevelop game!"); 
-        }
+module.exports.installGDMod = function (outputDir) {
+  const outputDirFiles = fs.readdirSync(outputDir);
 
-        // Check if it already got patched
-        if (outputDirFiles.includes("GDApi.js")) { 
-            console.error(chalk.redBright("The given output path contains an already patched game!")); 
-            return Promise.reject("The given output path contains an already patched game!"); 
-        }
+  // Check if it is a GDevelop game
+  if (!outputDirFiles.includes("gd.js")) {
+    console.error(
+      chalk.redBright("The given output path is not a GDevelop game!")
+    );
+    return Promise.reject("The given output path is not a GDevelop game!");
+  }
 
-        // Copy files over
-        const copyFiles = function() {
-            return new Promise((resolve) => {
-                let finished = 0;
-                for(let file of APIDeps) {
-                    fs.readFile(file, (error, fileInMemory) => {
-                        const endpath = path.join(outputDir, file);
-                        console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.green("Adding file ") + chalk.italic(chalk.grey(endpath)) + chalk.green("..."));
-                        fs.writeFile(endpath, fileInMemory, () => {});
+  // Check if it already got patched
+  if (outputDirFiles.includes("GDApi.js")) {
+    console.error(
+      chalk.redBright("The given output path contains an already patched game!")
+    );
+    return Promise.reject(
+      "The given output path contains an already patched game!"
+    );
+  }
 
-                        if(++finished === APIDeps.length) { resolve(); }
-                    });
-                }
-            });
-        }
+  // Copy files over
+  const copyFiles = function () {
+    return new Promise((resolve) => {
+      let finished = 0;
+      for (let file of APIDeps) {
+        fs.readFile(file, (error, fileInMemory) => {
+          const endpath = path.join(outputDir, file);
+          console.log(
+            chalk.greenBright("[BASE PATCHER] ") +
+              chalk.green("Adding file ") +
+              chalk.italic(chalk.grey(endpath)) +
+              chalk.green("...")
+          );
+          fs.writeFile(endpath, fileInMemory, () => {});
 
-        return copyFiles()
-        .then(() => { // Get RuntimeGame access
-            let runtimeGameFile = String(fs.readFileSync(path.join(outputDir, "runtimegame.js")));
-            runtimeGameFile = runtimeGameFile.replace(
-                "gdjs.RuntimeGame = function(data, options) {", 
-`gdjs.RuntimeGame = function(data, options) {
-  if(typeof GDAPI === "undefined") window.GDAPI = {};
-  GDAPI.game = this;`);
-            fs.writeFileSync(path.join(outputDir, "runtimegame.js"), runtimeGameFile);
-            console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied RuntimeGame access patch."));
-        })
-        .then(() => { // Add Includes for API
-            let indexFile = String(fs.readFileSync(path.join(outputDir, "index.html")));
-            // An include file is used to determine the loading order and what needs to be loaded.
-            for(let include of APIDeps) {
-                indexFile = insertInclude(indexFile, include);
-            }
-            fs.writeFileSync(path.join(outputDir, "index.html"), indexFile);
-            console.log(chalk.greenBright("[BASE PATCHER] ") + chalk.magenta("Applied dependency includes patch to index.html."));
+          if (++finished === APIDeps.length) {
+            resolve();
+          }
         });
-}
+      }
+    });
+  };
+
+  return copyFiles()
+    .then(() => {
+      // Get RuntimeGame access
+      let runtimeGameFile = String(
+        fs.readFileSync(path.join(outputDir, "runtimegame.js"))
+      );
+      runtimeGameFile = runtimeGameFile.replace(
+        "gdjs.RuntimeGame = function(data, options) {",
+        `gdjs.RuntimeGame = function(data, options) {
+  if(typeof GDAPI === "undefined") window.GDAPI = {};
+  GDAPI.game = this;`
+      );
+      fs.writeFileSync(path.join(outputDir, "runtimegame.js"), runtimeGameFile);
+      console.log(
+        chalk.greenBright("[BASE PATCHER] ") +
+          chalk.magenta("Applied RuntimeGame access patch.")
+      );
+    })
+    .then(() => {
+      // Add Includes for API
+      let indexFile = String(
+        fs.readFileSync(path.join(outputDir, "index.html"))
+      );
+      // An include file is used to determine the loading order and what needs to be loaded.
+      for (let include of APIDeps) {
+        indexFile = insertInclude(indexFile, include);
+      }
+      fs.writeFileSync(path.join(outputDir, "index.html"), indexFile);
+      console.log(
+        chalk.greenBright("[BASE PATCHER] ") +
+          chalk.magenta("Applied dependency includes patch to index.html.")
+      );
+    });
+};
